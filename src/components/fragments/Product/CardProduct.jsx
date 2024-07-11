d9c8e5d74d675b88aba9e8ac22b3192d0ce724c8
unknown <kemalcrisannnaufal@gmail.com>
2024-07-11 10:33:14 +0700

    Fix Size CardProduct

--- src/components/fragments/Product/CardProduct.jsx
+++ src/components/fragments/Product/CardProduct.jsx
@@ -8,7 +8,7 @@ const CardProduct = (props) => {
   const { children, url } = props;
   return (
     <Link to={url}>
-      <div className="w-full max-w-xs border rounded-lg bg-cover shadow-lg">
+      <div className="w-full max-w-56 border rounded-lg bg-cover shadow-lg">
         {children}
       </div>
     </Link>
@@ -17,14 +17,14 @@ const CardProduct = (props) => {
 
 CardProduct.propTypes = {
   children: PropTypes.node,
-  url: PropTypes.string.isRequired
+  url: PropTypes.string.isRequired,
 };
 
 const Header = (props) => {
   const { image } = props;
   return (
-    <div className="bg-green-500 h-[300px]">
-      <img src={image} alt="" className="object-cover h-[300px]" />
+    <div className="bg-green-500 h-[200px]">
+      <img src={image} alt="" className="object-cover w-full h-full" />
     </div>
   );
 };
@@ -47,9 +47,9 @@ const Body = (props) => {
   } = props;
   return (
     <>
-      <div className="w-full p-4 pb-10 flex flex-col gap-1">
-        <h5 className="line-clamp-2 text-lg text-black font-medium">{title}</h5>
-        <h2 className="text-xl font-bold">
+      <div className="w-full p-4 pb-5 flex flex-col">
+        <h5 className="line-clamp-2 text-sm text-black font-normal">{title}</h5>
+        <h2 className="text-md font-bold">
           {price.toLocaleString("id-ID", {
             style: "currency",
             currency: "IDR",
@@ -57,33 +57,38 @@ const Body = (props) => {
         </h2>
 
         {priceBefore !== 0 && (
-          <div className="flex gap-2 my-1">
-            <p className="line-through text-slate-400 font-semibold">
-              {priceBefore}
+          <div className="flex gap-2 my-1 text-xs">
+            <p className=" line-through text-slate-400 font-semibold">
+              {priceBefore.toLocaleString("id-ID", {
+                style: "currency",
+                currency: "IDR",
+              })}
             </p>
             <strong className="text-red-500">{discountPriceBefore}%</strong>
           </div>
         )}
 
-        <div className="w-[110px] py-1 px-2 bg-green-200 flex justify-center items-center rounded">
-          <p className="font-bold text-sm text-green-600">
-            Diskon {discount / 1000} Rb
-          </p>
+        <div className="max-w-24">
+          <div className="py-1 px-1 bg-green-200 flex justify-center items-center rounded mt-1">
+            <p className="font-bold text-xs text-green-600">
+              Diskon {discount / 1000} Rb
+            </p>
+          </div>
         </div>
 
-        <div className="flex items-center gap-2 text-md text-slate-500 font-semibold">
+        <div className="flex items-center gap-2 text-xs text-slate-500 font-semibold mt-2">
           <img
             src={isStarSeller ? StarSellerIcon : StoreIcon}
             alt="*"
-            className="h-5 w-5"
+            className="h-4 w-4"
           />
           <div>
             <p>{place}</p>
           </div>
         </div>
 
-        <div className="flex items-center gap-2 text-md text-slate-500 font-semibold">
-          <img src={StarIcon} alt="*" className="h-5 w-5" />
+        <div className="flex items-center gap-2 text-xs text-slate-500 font-semibold mt-2">
+          <img src={StarIcon} alt="*" className="h-4 w-4" />
           <div>
             <p>
               {rating.toFixed(1)} | {sold}+ terjual
